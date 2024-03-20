83db251be6faa784ed4dcfe92b28e43c3f28d2b3
raiderrXD <163460083+raiderrXD@users.noreply.github.com>
2024-03-20 22:03:37 +0300

    Create Kurulum.py

---
+++ Kurulum.py
@@ -0,0 +1,12 @@
+#import os
+#import requests
+#import socket
+#import random
+#import webbrowser
+
+os.system ("pip install requests")
+os.system ("pip install socket")
+os.system ("pip install random")
+os.system ("pip install webbrowser")
+os.system ("pip install os")
+os.system ("pyfiglet")
