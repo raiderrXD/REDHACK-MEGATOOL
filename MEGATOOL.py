ea3e3bbccdbc7fcbb471087a437a4bf2e38bb1eb
raiderrXD <163460083+raiderrXD@users.noreply.github.com>
2024-03-20 19:44:07 +0300

    Update MEGATOOL.py

--- MEGATOOL.py
+++ MEGATOOL.py
@@ -7,3 +7,26 @@ os.system('clear')
 import pyfiglet 
 a= pyfiglet.figlet_format("Red Hack", font = "slant"  ) 
 print(a)
+
+print('\033[0;31m         THE TOOL BY RED HACK ARSİV √')
+print("""\033[1;31m
+   _____________________________________________
+   |İnstagram:_y1git.inc                       |
+   |Tele:https://t.me/redhackarsiv             |
+   |Tele Team:https://t.me/redhackchet         |
+   |___________________________________________|
+""")
+print('\033[0;35m   _________________________________________________')
+print("""\033[1;36m \033[3m
+  [1]=>  Metasploite         [9]=>   Osif
+  [2]=>  Nmap                [10]=>  Admin Hack
+  [3]=>  Sqlmap              [11]=>  Cam Hack
+  [4]=>  PyPhisher           [12]=>  GhostTRACK
+  [5]=>  Kali Nethunter      [13]=>  Git info Victam
+  [6]=>  Yarb DDoS           [14]=>  Android App Virus
+  [7]=>  Red Hawk            [15]=>  bo_hyd_cat
+  [8]=>  Fsociety            [16]=>  Parrot Banner
+
+         [99]=> rest       [0]=>  about Tool
+""")
+print('\033[0;35m   _________________________________________________                        ')
